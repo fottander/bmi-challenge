958ee527e453f8cc41d20f9b6a7f1ebdda985c1e
felixottander <felix.ottander@yahoo.com>
2017-02-14 15:41:42 +0100

    added jasmine-jquery.js file and a bmi.ui spec file. Also created the index.html file with the basic structure and a script to run the bmi calculator.

---
+++ spec/bmi_ui_spec.js
@@ -0,0 +1,7 @@
+describe('BMI-UI - index.html', function(){
+  beforeEach(function() {
+    jasmine.getFixtures().fixturesPath = ',';
+    loadFixtures('index.html');
+    $.holdReady(false);
+  });
+});
