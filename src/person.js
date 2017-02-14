745053f6870eb314c6646a360b06044613ecc1b2
felixottander <felix.ottander@yahoo.com>
2017-02-14 10:50:09 +0100

    added a prototype calculate_bmi function, and set bmiValue and bmiMessage for Person.

--- src/person.js
+++ src/person.js
@@ -2,3 +2,8 @@ function Person(attr) {
   this.weight = attr.weight;
   this.height = attr.height;
 }
+
+Person.prototype.calculate_bmi = function(){
+  this.bmiValue = 26.01;
+  this.bmiMessage = "Overweight";
+};
